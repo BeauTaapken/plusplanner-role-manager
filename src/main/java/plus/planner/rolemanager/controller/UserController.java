aaede824127428088547a34595a4c22062dce34a
Niek125 <niek.sleddens@gmail.com>
2020-01-22 14:30:01 +0100

    adding users

--- src/main/java/plus/planner/rolemanager/controller/UserController.java
+++ src/main/java/plus/planner/rolemanager/controller/UserController.java
@@ -1,15 +1,14 @@
 package plus.planner.rolemanager.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import plus.planner.rolemanager.model.User;
 import plus.planner.rolemanager.repository.UserRepository;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,17 +16,12 @@ import java.util.List;
 public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserRepository userrepo;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public UserController(UserRepository userrepo) {
+    public UserController(UserRepository userrepo, ObjectMapper objectMapper) {
         this.userrepo = userrepo;
-    }
-
-    @RequestMapping("/create")
-    public void createUser(@RequestBody User user) {
-        logger.info("saving user: " + user.getUserid());
-        userrepo.save(user);
-        logger.info("saved user");
+        this.objectMapper = objectMapper;
     }
 
     @RequestMapping("/read/{projectid}")
@@ -38,8 +32,9 @@ public class UserController {
         return users;
     }
 
-    @RequestMapping("/update")
-    public void updateUser(@RequestBody User user) {
+    @PostMapping("/save")
+    public void updateUser(@RequestBody String usr) throws IOException {
+        final User user = objectMapper.readValue(usr, User.class);
         logger.info("saving user: " + user.getUserid());
         userrepo.save(user);
         logger.info("saved user");
